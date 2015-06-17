cefce3218f0c9033b99387afe97ae8ec7764e26f
gvit00702 <gvit00702@gmail.com>
2015-06-17 11:06:47 +0800

    Added native dialog plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -11,6 +11,20 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -18,7 +32,8 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.0.0",
     "cordova-plugin-console": "1.0.0",
-    "cordova-plugin-device": "1.0.0"
+    "cordova-plugin-device": "1.0.0",
+    "cordova-plugin-dialogs": "1.1.0"
 }
 // BOTTOM OF METADATA
 });
