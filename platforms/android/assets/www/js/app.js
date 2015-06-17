e0aa60c9b1b4ee8c4f7a699d0a903f0035411259
gvit00702 <gvit00702@gmail.com>
2015-06-17 12:22:02 +0800

    Added PageSlider (Hardware acceleration) function

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -9,15 +9,17 @@
         Handlebars.compile($("#employee-list-tpl").html());
     EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
+    var slider = new PageSlider($('body'));
+
     service.initialize().done(function() {
         // console.log("Service initialized");
         router.addRoute('', function() {
-            $('body').html(new HomeView(service).render().$el);
+            slider.slidePage(new HomeView(service).render().$el);
         });
 
         router.addRoute('employees/:id', function(id) {
             service.findById(parseInt(id)).done(function(employee) {
-                $('body').html(new EmployeeView(employee).render().$el);
+                slider.slidePage(new EmployeeView(employee).render().$el);
             });
         });
 
