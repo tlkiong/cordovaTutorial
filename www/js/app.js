5112f4806982f9177376c36305c910cfb06e41d3
gvit00702 <gvit00702@gmail.com>
2015-06-17 12:17:23 +0800

    Added view routing for employee details

--- www/js/app.js
+++ www/js/app.js
@@ -7,10 +7,21 @@
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template =
         Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
     service.initialize().done(function() {
         // console.log("Service initialized");
-        $('body').html(new HomeView(service).render().$el);
+        router.addRoute('', function() {
+            $('body').html(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+            service.findById(parseInt(id)).done(function(employee) {
+                $('body').html(new EmployeeView(employee).render().$el);
+            });
+        });
+
+        router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
