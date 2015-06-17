8e597e2f47ddb2fedaf5cba82ab79889089dc612
gvit00702 <gvit00702@gmail.com>
2015-06-17 12:09:46 +0800

    Added employee-tpl

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,46 +1,34 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
-(function () {
+(function() {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
 
-    var homeTpl = Handlebars.compile($("#home-tpl").html());
-    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+    HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
+    EmployeeListView.prototype.template =
+        Handlebars.compile($("#employee-list-tpl").html());
 
-    service.initialize().done(function () {
+    service.initialize().done(function() {
         // console.log("Service initialized");
-        renderHomeView();
+        $('body').html(new HomeView(service).render().$el);
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    // $('.search-key').on('keyup', findByName);
-    // $('.help-btn').on('click', function() {
-    //     alert("Employee Directory v3.4");
-    // });
 
-    document.addEventListener('deviceready', function () {
+    document.addEventListener('deviceready', function() {
         if (navigator.notification) { // Override default HTML alert with native dialog
-            window.alert = function (message) {
+            window.alert = function(message) {
                 navigator.notification.alert(
-                    message,    // message
-                    null,       // callback
+                    message, // message
+                    null, // callback
                     "Workshop", // title
-                    "OK"        // buttonName
+                    "OK" // buttonName
                 );
             };
         }
-        
+
         FastClick.attach(document.body);
     }, false);
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        service.findByName($('.search-key').val()).done(function (employees) {
-            $('.content').html(employeeListTpl(employees));
-        });
-    }
 
-    function renderHomeView() {
-        $('body').html(homeTpl());
-        $('.search-key').on('keyup', findByName);
-    }
 }());
