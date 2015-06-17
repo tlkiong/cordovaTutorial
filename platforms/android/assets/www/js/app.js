6a1b33dcb92a18b88a1af7548db275cf951bdf33
gvit00702 <gvit00702@gmail.com>
2015-06-17 11:49:04 +0800

    Fixed a typo issue on home-tpl

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,15 +3,20 @@
 
     /* ---------------------------------- Local Variables ---------------------------------- */
     var service = new EmployeeService();
+
+    var homeTpl = Handlebars.compile($("#home.tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
     service.initialize().done(function () {
-        console.log("Service initialized");
+        // console.log("Service initialized");
+        renderHomeView();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Employee Directory v3.4");
-    });
+    // $('.search-key').on('keyup', findByName);
+    // $('.help-btn').on('click', function() {
+    //     alert("Employee Directory v3.4");
+    // });
 
     document.addEventListener('deviceready', function () {
         if (navigator.notification) { // Override default HTML alert with native dialog
@@ -30,14 +35,12 @@
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.content').html(employeeListTpl(employees));
         });
     }
 
+    function renderHomeView() {
+        $('body').html(homeTpl());
+        $('.search-key').on('keyup', findByName);
+    }
 }());
